32157157acbf7e82d02563bef5f47a64c1d46178
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-12-02 13:51:20 +0100

    Main method with array and get the smallest value

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 	public static void main(String[] args) {
-
+		int[] array = {23,12,23,44,22,4,54,23,54,23,3,33,43,77,45,23,12,2,76};
+		
+		for(int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+		
+		System.out.println("Length: " + array.length);
+		System.out.println("Output: " + min(array));
 	}
 	
 	//Returns the min value in the sequence, if the sequence is empty throw IllegalArgumentException
@@ -25,7 +32,13 @@ public class Main {
 			j = 0;
 			while (j < pairs)
 			{
-				partSequence[j++] = (sequence[i] < sequence[i + 1])? sequence[i] : sequence[i + 1];
+				if(sequence[i] < sequence[i+1]) {
+					partSequence[j++] = sequence[i];
+				}
+				else {
+					partSequence[j++] = sequence[i+1];
+				}
+				
 				i += 2;
 			}
 			if (unpairedElements == 1) {
